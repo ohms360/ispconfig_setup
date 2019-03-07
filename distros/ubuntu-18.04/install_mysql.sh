84eb0ade24fe6b353884c74dbb5940b2743033b3
Ciaran Owens <hello@ciaranowens.com>
2019-03-07 23:38:15 +0000

    Updated to support MariaDB 10.3 conf path

--- distros/ubuntu-18.04/install_mysql.sh
+++ distros/ubuntu-18.04/install_mysql.sh
@@ -20,7 +20,7 @@ InstallSQLServer() {
     echo "mysql-server-5.5 mysql-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
     echo "mysql-server-5.5 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
     apt_install mariadb-client mariadb-server
-    sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/mariadb.conf.d/50-server.cnf
+    sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/my.cnf
     echo -e "[${green}DONE${NC}]\n"
     echo -n "Restarting MariaDB... "
     service mysql restart
